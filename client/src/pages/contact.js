205ece256726de1569b5f68ac3b209705956576d
Akash-Chopade <akashchopade1000@gmail.com>
2024-12-01 20:48:30 +0530

    URL change

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -16,7 +16,7 @@ function Contact() {
     setStatus('Sending...');
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://portfolio-website-3o0p.onrender.com/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
