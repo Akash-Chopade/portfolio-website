61610264f0c462075489d952c7c386fa996a3667
Akash-Chopade <67311976+Akash-Chopade@users.noreply.github.com>
2025-03-31 21:24:54 +0530

    Update header.js

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -25,7 +25,7 @@ function Header() {
         <ul className={`nav-bar ${isMenuOpen ? 'open' : ''}`}>
           <li><a href="#home" onClick={closeMenu}>Home</a></li>
           <li><a href="#about" onClick={closeMenu}>About</a></li>
-          <li><a href="#experince" onClick={closeMenu}>Experince</a></li>
+          <li><a href="#experince" onClick={closeMenu}>Experience</a></li>
           <li><a href="#projects" onClick={closeMenu}>Projects</a></li>
           <li><a href="#contact" onClick={closeMenu}>Contact</a></li>
         </ul>
